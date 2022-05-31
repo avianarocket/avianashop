d4ae23e38230db9c63f746d45e7b2be3704331b3
Anderson A. Viana <avianarocket@hotmail.com>
2022-05-31 11:35:01 -0300

    autoload do Hcode Class

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    '' => array($vendorDir . '/avianarocket/php-classes/src'),
+    'Hcode\\' => array($vendorDir . '/hcodebr/php-classes/src'),
 );
