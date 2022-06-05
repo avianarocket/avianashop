1ad54e73cb7fe9e3a79841412d54f662c054ba2a
Anderson A. Viana <avianarocket@hotmail.com>
2022-06-05 19:10:51 -0300

    Carrinho Produtos

--- R-site-cart.php
+++ R-site-cart.php
@@ -2,6 +2,7 @@
 
 use \Hcode\Page;
 use \Hcode\Model\Cart;
+use Hcode\Model\Product;
 
 //rota produtos home
 $app->get("/cart", function(){
@@ -15,6 +16,57 @@ $app->get("/cart", function(){
 		'products'=>$cart->getProducts(),
 		'error'=>Cart::getMsgError()
 	]);
+
+});
+
+$app->get("/cart/:idproduct/add", function($idproduct)
+{
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$qtd = (isset($_GET['qtd'])) ? (int)$_GET['qtd'] : 1;
+
+	for ($i = 0; $i < $qtd; $i++) { 
+		$cart->addProduct($product);
+	}
+
+	//$cart->addProduct($product);
+
+	header("Location: /cart");
+	exit;
+
+});
+
+$app->get("/cart/:idproduct/minus", function($idproduct)
+{
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$cart->removeProduct($product);
+
+	header("Location: /cart");
+	exit;
+
+});
+
+$app->get("/cart/:idproduct/remove", function($idproduct)
+{
+	$product = new Product();
+
+	$product->get((int)$idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$cart->removeProduct($product, true);
+
+	header("Location: /cart");
+	exit;
 
 });
 
