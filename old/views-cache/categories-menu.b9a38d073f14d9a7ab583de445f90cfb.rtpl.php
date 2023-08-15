9a10ca827c36547734db86a1b014e3c22b003111
Anderson A. Viana <52810069+avianarocket@users.noreply.github.com>
2023-08-15 16:09:13 -0300

    Delete old directory

--- old/views-cache/categories-menu.b9a38d073f14d9a7ab583de445f90cfb.rtpl.php
+++
@@ -1,1 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><li><a href="/categories/10">Android</a></li><li><a href="/categories/6">Apple</a></li><li><a href="/categories/12">Esculturas</a></li><li><a href="/categories/8">Motorola</a></li><li><a href="/categories/9">Samsung</a></li>
